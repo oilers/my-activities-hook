46156966b2939935f8ed9744e690d60ad763e016
Kodi <soiler@forsythe.com>
2014-08-25 20:50:35 -0600

    Removing BlogAction as it's unneeded. Also, use log

--- docroot/WEB-INF/src/org/oilers/liferay/activities/hook/LoginAction.java
+++ docroot/WEB-INF/src/org/oilers/liferay/activities/hook/LoginAction.java
@@ -1,41 +1,40 @@
 package org.oilers.liferay.activities.hook;
 
-import java.util.Enumeration;
-
-import javax.portlet.RenderRequest;
-import javax.portlet.RenderResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.liferay.portal.kernel.events.Action;
 import com.liferay.portal.kernel.events.ActionException;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.model.User;
 import com.liferay.portal.service.UserLocalServiceUtil;
 import com.liferay.portal.util.PortalUtil;
+import com.liferay.portlet.expando.model.ExpandoColumnConstants;
 
 public class LoginAction extends Action {
+
+	private static final String LOGIN_PROPERTY = "logins";
+	private static final Log log = LogFactoryUtil.getLog(LoginAction.class);
 
 	@Override
 	public void run(HttpServletRequest request, HttpServletResponse response)
 			throws ActionException {
-		System.out.println("Login Hook hit.");
+		log.info("*********************************************");
+		log.info("* Login Hook hit.                           *");
+		log.info("*********************************************");
 		try {
 			User user = PortalUtil.getUser(request);
-			Integer logins = (Integer)user.getExpandoBridge().getAttribute("logins");
-			user.getExpandoBridge().setAttribute("logins", logins + 1);
+			if(!user.getExpandoBridge().hasAttribute(LOGIN_PROPERTY)) {
+				user.getExpandoBridge().addAttribute(LOGIN_PROPERTY, ExpandoColumnConstants.INTEGER, 0);
+			}
+			Integer logins = (Integer)user.getExpandoBridge().getAttribute(LOGIN_PROPERTY);
+			user.getExpandoBridge().setAttribute(LOGIN_PROPERTY, logins + 1);
 			UserLocalServiceUtil.updateUser(user);
-		} catch (PortalException | SystemException e) {
-			e.printStackTrace();
+		} catch (Exception e) {
+			log.error(e);
 		}
+	}
 
-	}
-	
-	@Override
-	public void run(RenderRequest renderRequest, RenderResponse renderResponse)
-			throws ActionException {
-		super.run(renderRequest, renderResponse);
-	}
 
 }
